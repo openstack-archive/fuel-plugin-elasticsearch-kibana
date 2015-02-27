74d140970a9c141d4d7046f95ebfa51b2d19aae0
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-02-27 15:29:16 +0100

    Initial import
    Current limitations:
    
    - Kibana is not installed yet.
    - User must provide dedicated disk(s) for data storage.
    - CentOS not supported yet.
    
    Change-Id: Iac7e0fea4d8e1649452f232a7ca096bb7dfe28a8

---
+++ deployment_scripts/puppet/modules/elasticsearch_kibana/manifests/init.pp
@@ -0,0 +1,46 @@
+class elasticsearch_kibana (
+    $pv_name,
+) {
+    include elasticsearch_kibana::params
+
+    # Creates the logical volume
+    lvm::volume { $elasticsearch_kibana::params::lv_name:
+      alias  => 'elasticsearch',
+      ensure => present,
+      vg     => $elasticsearch_kibana::params::vg_name,
+      pv     => $pv_name,
+      fstype => $elasticsearch_kibana::params::fstype,
+    }
+
+    # create the directory
+    file { $elasticsearch_kibana::params::es_dir:
+      alias  => 'es_dir',
+      ensure => directory,
+    }
+
+    # Mount the directory
+    mount { $elasticsearch_kibana::params::es_dir:
+      alias   => 'es_dir',
+      device  => $elasticsearch_kibana::params::device,
+      ensure  => mounted,
+      fstype  => $elasticsearch_kibana::params::fstype,
+      options => "defaults",
+      require => [File['es_dir'], Lvm::Volume['elasticsearch']],
+    }
+
+    # Ensure that java is installed
+    package { $elasticsearch_kibana::params::java:
+      alias  => 'java',
+      ensure => installed,
+    }
+
+    # Install elasticsearch
+    class { "elasticsearch":
+      datadir => ["${elasticsearch_kibana::params::es_dir}/elasticsearch_data"],
+      require => [Mount['es_dir'], Package['java']],
+    }
+
+    # Start an instance of elasticsearch
+    elasticsearch::instance { $elasticsearch_kibana::params::es_instance: }
+}
+
