8d2145f1fb100c2e16a46b735b225ee6561d618f
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-04 17:55:59 +0100

    Install Kibana.
    - We are using nginx as the HTTP server.
    
    Change-Id: Ia8ac64f8df9ea65ed4f0b608aa971907154cd5be

--- deployment_scripts/puppet/modules/kibana/manifests/init.pp
+++ deployment_scripts/puppet/modules/kibana/manifests/init.pp
@@ -1,5 +1,33 @@
-class kibana
-{
- # Work is in progress
-}
+class kibana {
 
+    $kibana_dir = '/opt/kibana'
+    $kibana_conf = "${kibana_dir}/config.js"
+    $kibana_dash = "${kibana_dir}/app/dashboards/logs.json"
+
+    # Deploy kibana
+    file { $kibana_dir:
+      source  => "puppet:///modules/kibana/src",
+      recurse => true,
+    }
+
+    # Replace config.js and add logs.json.
+    file { $kibana_conf:
+      ensure  => file,
+      content => template('kibana/config.js.erb'),
+      require => File[$kibana_dir],
+    }
+
+    file { $kibana_dash:
+      ensure  => file,
+      content => template('kibana/logs.json.erb'),
+      require => File[$kibana_dir],
+    }
+
+    # Install nginx
+    class { 'nginx':
+      manage_repo           => false,
+      nginx_vhosts          => { 'kibana.local' => { 'www_root' => $kibana_dir } },
+      nginx_vhosts_defaults => { 'listen_options' => 'default_server' },
+      require               => File[$kibana_conf],
+    }
+}
