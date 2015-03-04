4f25b54bf8d765e536473cfd2461073e887ed49f
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-04 16:53:34 +0100

    Restructuring manifests.
    - Renamed elasticsearch_kibana into kibana
    - Now kibana manifests only holds kibana installation and configuration
    - Move all configuration of elasticsearch into its own file that will be
      called from tasks.
    
    Change-Id: Ief27cc15b2498330446c1537aa0662bf84333048

---
+++ deployment_scripts/puppet/modules/kibana/manifests/init.pp
@@ -0,0 +1,5 @@
+class kibana
+{
+ # Work is in progress
+}
+
--- deployment_scripts/puppet/site.pp
+++
@@ -1,7 +0,0 @@
-$fuel_settings = parseyaml(file('/etc/astute.yaml'))
-
-$disks = regsubst($fuel_settings['elasticsearch_kibana']['dedicated_disks'], '([a-z]+)', '/dev/\1', 'G')
-
-class { 'elasticsearch_kibana':
-    pv_name => split($disks, ','),
-}
