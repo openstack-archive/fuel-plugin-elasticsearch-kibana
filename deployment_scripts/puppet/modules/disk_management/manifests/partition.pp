5e878fba1bfb1f8f90581af5f0f3598b154d8336
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-05 11:06:41 +0100

    Add support for CentOS.
    - Install openjdk 1.8.0 headless.
    - Manage the /boot that is mounted as RAID 1 on all available disks.
    
    Change-Id: Ie346a466132d0dabc2eda61d4ac65ebd50b78222

--- deployment_scripts/puppet/modules/disk_management/manifests/partition.pp
+++ deployment_scripts/puppet/modules/disk_management/manifests/partition.pp
@@ -1,1 +1,32 @@
-define disk_management::partition {}
+define disk_management::partition {
+
+  $disk = $title
+  $script = "/usr/local/bin/add_partition_on_raid.sh"
+  $cmd  = "${script} ${disk}"
+
+  case $::osfamily {
+    'RedHat': {
+      # CentOS deploys /boot into a RAID on all available disks. So in
+      # this case we need to create a new partition instead of using the whole
+      # disks as we do for Debian family.
+
+      package { 'parted':
+        ensure => installed,
+      }
+
+      file { $script:
+        ensure  => 'file',
+        source  => 'puppet:///modules/disk_management/add_partition_on_raid.sh',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0700',
+        require => Package['parted'],
+      }
+
+      exec { 'run_script':
+        command => $cmd,
+        require => File[$script],
+      }
+    }
+  }
+}
