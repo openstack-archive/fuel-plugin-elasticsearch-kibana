b97059be97594ed19f3b5bcfcfac5f4656de621e
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-09 15:37:49 +0100

    Create disk paritions on Ubuntu.
    Now we create a partition /dev/sdb1 instead of using the whole disk.
    
    Change-Id: I8b5bdde546858d1e4ad9fc30719415453a7268ab

--- deployment_scripts/puppet/modules/disk_management/manifests/partition.pp
+++ deployment_scripts/puppet/modules/disk_management/manifests/partition.pp
@@ -1,32 +1,26 @@
 define disk_management::partition {
 
-  $disk = $title
-  $script = "/usr/local/bin/add_partition_on_raid.sh"
-  $cmd  = "${script} ${disk}"
+  $disk            = $title
+  $script          = "add_partition.sh"
+  $puppet_source   = "puppet:///modules/disk_management/${script}"
+  $script_location = "/usr/local/bin/${script}"
+  $cmd             = "${script_location} ${disk}"
 
-  case $::osfamily {
-    'RedHat': {
-      # CentOS deploys /boot into a RAID on all available disks. So in
-      # this case we need to create a new partition instead of using the whole
-      # disks as we do for Debian family.
+  package { 'parted':
+    ensure => installed,
+  }
 
-      package { 'parted':
-        ensure => installed,
-      }
+  file { $script_location:
+    ensure  => 'file',
+    source  => $puppet_source,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    require => Package['parted'],
+  }
 
-      file { $script:
-        ensure  => 'file',
-        source  => 'puppet:///modules/disk_management/add_partition_on_raid.sh',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0700',
-        require => Package['parted'],
-      }
-
-      exec { 'run_script':
-        command => $cmd,
-        require => File[$script],
-      }
-    }
+  exec { 'run_script':
+    command => $cmd,
+    require => File[$script_location],
   }
 }
