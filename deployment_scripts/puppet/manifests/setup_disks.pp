d8cde056aaabe64c0d66ad8e5ac7ca2621ea18e6
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-09 15:37:49 +0100

    Create disk partitions on Ubuntu.
    Now we create a partition /dev/sdb1 instead of using the whole disk.
    
    Change-Id: I8b5bdde546858d1e4ad9fc30719415453a7268ab

--- deployment_scripts/puppet/manifests/setup_disks.pp
+++ deployment_scripts/puppet/manifests/setup_disks.pp
@@ -3,11 +3,11 @@ $fuel_settings = parseyaml(file('/etc/astute.yaml'))
 if $fuel_settings['elasticsearch_kibana']['node_name'] == $fuel_settings['user_node_name'] {
 
   $disks = regsubst($fuel_settings['elasticsearch_kibana']['dedicated_disks'], '([a-z]+)', '/dev/\1', 'G')
+  $array_disks = split($disks, ',')
 
-  class  { 'disk_management':
-    disks     => split($disks, ','),
-    directory => $fuel_settings['elasticsearch_kibana']['data_dir'],
-    lv_name   => "es",
-    vg_name   => "data",
+  class { 'disk_management': }
+
+  disk_management::partition { $array_disks:
+    require => Class['disk_management']
   }
 }
