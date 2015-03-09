84976a55aed8a046ba07f0f0ce49b1738474d518
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-09 11:29:43 +0100

    Deploy the plugin on the selected node only.
    Currently Elasticsearch and Kibana are deployed on all nodes that are
    using base-os role. This change ensures that the plugin is only executed
    on the node selected through the Fuel UI.
    
    Change-Id: I1a621d4b453c703e4ba92ba40c0659ccb2fc041b

--- deployment_scripts/puppet/manifests/kibana.pp
+++ deployment_scripts/puppet/manifests/kibana.pp
@@ -1,1 +1,5 @@
-class { 'lma_logging_analytics::kibana': }
+$fuel_settings = parseyaml(file('/etc/astute.yaml'))
+
+if $fuel_settings['elasticsearch_kibana']['node_name'] == $fuel_settings['user_node_name'] {
+  class { 'lma_logging_analytics::kibana': }
+}
