fe6420a1bae69b14489131aef1738dbd99a48379
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-09 11:29:43 +0100

    Deploy stuff only on the specified node.
    Currently Elasticsearch and kibana are deployed on all nodes that are
    using base-os role. This patch check if the name of the node is the one
    that is given through the Fuel UI.
    
    Change-Id: I1a621d4b453c703e4ba92ba40c0659ccb2fc041b

--- deployment_scripts/puppet/manifests/setup_disks.pp
+++ deployment_scripts/puppet/manifests/setup_disks.pp
@@ -1,10 +1,13 @@
 $fuel_settings = parseyaml(file('/etc/astute.yaml'))
 
-$disks = regsubst($fuel_settings['elasticsearch_kibana']['dedicated_disks'], '([a-z]+)', '/dev/\1', 'G')
+if $fuel_settings['elasticsearch_kibana']['node_name'] == $fuel_settings['user_node_name'] {
 
-class  { 'disk_management':
+  $disks = regsubst($fuel_settings['elasticsearch_kibana']['dedicated_disks'], '([a-z]+)', '/dev/\1', 'G')
+
+  class  { 'disk_management':
     disks     => split($disks, ','),
     directory => $fuel_settings['elasticsearch_kibana']['data_dir'],
     lv_name   => "es",
     vg_name   => "data",
+  }
 }
