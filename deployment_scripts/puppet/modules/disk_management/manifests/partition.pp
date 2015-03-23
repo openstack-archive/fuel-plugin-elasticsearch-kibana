711fd4bcbe0669488aba3e75a0e8df9824465fe1
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-23 10:55:00 +0100

    Fix errors reported by puppet-lint
    This is required to enable the Fuel Plugins CI.
    
    Change-Id: I56aec27b6ad03c3794d03757238a21f598eeb8d1

--- deployment_scripts/puppet/modules/disk_management/manifests/partition.pp
+++ deployment_scripts/puppet/modules/disk_management/manifests/partition.pp
@@ -1,3 +1,7 @@
+# Class that partition the disk
+# If a disk is given as title we check for free space and allocated
+# this free space by calling the script given as parameter.
+
 define disk_management::partition {
 
   include disk_management::params
