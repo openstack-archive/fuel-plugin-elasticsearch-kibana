7a38152dfa2a3b462263e4c6bda05ea779741f22
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-23 10:55:00 +0100

    Fix errors reported by puppet-lint
    This is required to enable the Fuel Plugins CI.
    
    Change-Id: I56aec27b6ad03c3794d03757238a21f598eeb8d1

--- deployment_scripts/puppet/modules/disk_management/manifests/init.pp
+++ deployment_scripts/puppet/modules/disk_management/manifests/init.pp
@@ -1,8 +1,25 @@
+# Class disk_management
+#
+# This class is used to parition disks. This is achieved by running a
+# shell script.
+#
+# [* script *]
+#    The script called to partition disks. The script must be store
+#    in the files/ directory.
+
 class disk_management (
-  $script          = $disk_management::params::script,
-  $puppet_source   = $disk_management::params::puppet_source,
-  $script_location = $disk_management::params::script_location,
-) inherits disk_management::params {
+  $script = 'UNSET',
+) {
+
+  include disk_management::params
+
+  $script_real = $script ? {
+    'UNSET' => $disk_management::params::script,
+    default => $script
+  }
+
+  $puppet_source   = "puppet:///modules/disk_management/${script_real}"
+  $script_location = "/usr/local/bin/${script_real}"
 
   package { 'parted':
     ensure => installed,
