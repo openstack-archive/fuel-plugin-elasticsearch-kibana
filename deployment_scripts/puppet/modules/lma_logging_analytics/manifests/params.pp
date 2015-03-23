6402821fa76a40d1aa8d0fafa5caa3e5097e72e2
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-03-23 10:55:00 +0100

    Fix errors reported by puppet-lint
    This is required to enable the Fuel Plugins CI.
    
    Change-Id: I56aec27b6ad03c3794d03757238a21f598eeb8d1

--- deployment_scripts/puppet/modules/lma_logging_analytics/manifests/params.pp
+++ deployment_scripts/puppet/modules/lma_logging_analytics/manifests/params.pp
@@ -1,5 +1,7 @@
+# Class lma_logging_analytics::params
+
 class lma_logging_analytics::params {
-  $kibana_dir       = "/opt/kibana"
+  $kibana_dir       = '/opt/kibana'
   $kibana_config    = "${kibana_dir}/config.js"
   $kibana_dashboard = "${kibana_dir}/app/dashboards/logs.json"
 }
