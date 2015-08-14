639f8300d6e6d99ee9679f96bd008567628706a4
Simon Pasquier <spasquier@mirantis.com>
2015-08-14 11:37:02 +0200

    Add tests for curator and kibana classes
    Change-Id: I524c31ab4aae0cf528470ec1c0dad7b81f1ced97

---
+++ deployment_scripts/puppet/modules/lma_logging_analytics/spec/classes/lma_logging_analytics_curator_spec.rb
@@ -0,0 +1,40 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+require 'spec_helper'
+
+describe 'lma_logging_analytics::curator', :type => :class do
+    let(:facts) do
+        {:kernel => 'Linux', :operatingsystem => 'Ubuntu'}
+    end
+
+    describe 'with defaults' do
+        it { is_expected.to contain_package('python-elasticsearch-curator').with(
+            :ensure => 'installed'
+        )}
+
+        it { is_expected.not_to contain_cron('es-curator') }
+    end
+
+    describe 'with index prefixes and retention period > 0' do
+        let(:params) do
+            {:retention_period => 10, :prefixes => ['foo']}
+        end
+
+        it { is_expected.to contain_package('python-elasticsearch-curator').with(
+            :ensure => 'installed'
+        )}
+
+        it { is_expected.to contain_cron('es-curator') }
+    end
+end
