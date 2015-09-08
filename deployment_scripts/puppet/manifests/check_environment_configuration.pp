fe2e8948afa215e0e5e95ee9ff14585ae39be71f
Swann Croiset <scroiset@mirantis.com>
2015-09-08 10:26:33 +0200

    Switch to custom role elasticsearch_kibana
    - Use plugin version 3.0.0 and remove compatibility with MOS 6.1
    - Leverage common tasks to configure disk and network
    - Update README
    
    Change-Id: I7f185de0cfbe3098c9fee2d0f7a792df5b0a95e0

--- deployment_scripts/puppet/manifests/check_environment_configuration.pp
+++ deployment_scripts/puppet/manifests/check_environment_configuration.pp
@@ -14,32 +14,8 @@
 
 $elasticsearch_kibana = hiera('elasticsearch_kibana')
 
-if $elasticsearch_kibana['node_name'] == hiera('user_node_name') {
-  $roles = hiera('roles')
-  $blockdevices_array = split($::blockdevices, ',')
-
-  # Check that we're not colocated with other roles
-  if size($roles) > 1 {
-    fail('The Elasticsearch-Kibana plugin cannot be deployed with roles other than base-os.')
-  }
-
-  # Check that disk device(s) exist
-  if ($elasticsearch_kibana['disk1']) and !($elasticsearch_kibana['disk1'] in $blockdevices_array) {
-    fail("Disk device ${ elasticsearch_kibana['disk1'] } doesn't exist.")
-  }
-
-  if ($elasticsearch_kibana['disk2']) and !($elasticsearch_kibana['disk2'] in $blockdevices_array) {
-    fail("Disk device ${ elasticsearch_kibana['disk2'] } doesn't exist.")
-  }
-
-  if ($elasticsearch_kibana['disk3']) and !($elasticsearch_kibana['disk3'] in $blockdevices_array) {
-    fail("Disk device ${ elasticsearch_kibana['disk3'] } doesn't exist.")
-  }
-
-  # Check that JVM size doesn't exceed the physical RAM size
-  $jvmsize_mb = ($elasticsearch_kibana['jvm_heap_size'] + 0.0) * 1024
-  if $jvmsize_mb >= $::memorysize_mb {
-    fail("The configured JVM size (${ $elasticsearch_kibana['jvm_heap_size'] } GB) is greater than the total amount of RAM of the system (${ ::memorysize }).")
-  }
+# Check that JVM size doesn't exceed the physical RAM size
+$jvmsize_mb = ($elasticsearch_kibana['jvm_heap_size'] + 0.0) * 1024
+if $jvmsize_mb >= $::memorysize_mb {
+  fail("The configured JVM size (${ $elasticsearch_kibana['jvm_heap_size'] } GB) is greater than the total amount of RAM of the system (${ ::memorysize }).")
 }
-
