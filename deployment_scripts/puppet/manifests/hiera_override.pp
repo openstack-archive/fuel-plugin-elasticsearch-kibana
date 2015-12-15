99c184f3eb3a342cded8600828c99fc1e7c51a16
Swann Croiset <scroiset@mirantis.com>
2015-12-15 16:29:59 +0100

    Handle advanced settings for Elasticsearch
    These parameters are configured with default values if not provided:
    * number_of_replicas
    * minimum_master_nodes
    * recover_after_time
    * recover_after_nodes
    
    Change-Id: I3f4c1135c61b209e6e7b6160142c147721306cf9

--- deployment_scripts/puppet/manifests/hiera_override.pp
+++ deployment_scripts/puppet/manifests/hiera_override.pp
@@ -12,10 +12,40 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 #
-$hiera_dir        = '/etc/hiera/override'
-$plugin_name      = 'elasticsearch_kibana'
-$plugin_yaml      = "${plugin_name}.yaml"
-$corosync_roles   = [$plugin_name]
+$hiera_dir            = '/etc/hiera/override'
+$plugin_name          = 'elasticsearch_kibana'
+$plugin_yaml          = "${plugin_name}.yaml"
+$corosync_roles       = [$plugin_name]
+$elasticsearch_kibana = hiera_hash('elasticsearch_kibana')
+$network_metadata     = hiera('network_metadata')
+$es_nodes             = get_nodes_hash_by_roles($network_metadata, ['elasticsearch_kibana'])
+$es_nodes_count       = count($es_nodes)
+
+if is_integer($elasticsearch_kibana['number_of_replicas']) and $elasticsearch_kibana['number_of_replicas'] < $es_nodes_count {
+  $number_of_replicas = 0 + $elasticsearch_kibana['number_of_replicas']
+}else{
+  # Override the replication number otherwise this will lead to a stale cluster health
+  $number_of_replicas = $es_nodes_count - 1
+  notice("Set number_of_replicas to ${number_of_replicas}")
+}
+
+if is_integer($elasticsearch_kibana['minimum_master_nodes'] and $elasticsearch_kibana['minimum_master_nodes'] <= $es_nodes_count) {
+  $minimum_master_nodes = 0 + $elasticsearch_kibana['minimum_master_nodes']
+} elsif $es_nodes_count > 2 {
+  $minimum_master_nodes = floor($es_nodes_count / 2 + 1)
+}else{
+  $minimum_master_nodes = 1
+}
+notice("Set minimum_master_nodes to ${minimum_master_nodes}")
+
+$recover_after_time = 0 + $elasticsearch_kibana['recover_after_time']
+
+if is_integer($elasticsearch_kibana['recover_after_nodes']) and $elasticsearch_kibana['recover_after_nodes'] <= $es_nodes_count {
+  $recover_after_nodes = $elasticsearch_kibana['recover_after_nodes']
+} else {
+  $recover_after_nodes = floor($es_nodes_count * 2 / 3)
+  notice("Set recover_after_nodes to ${recover_after_nodes}")
+}
 
 $calculated_content = inline_template('
 corosync_roles:
@@ -23,6 +53,11 @@ corosync_roles:
 @corosync_roles.each do |crole|
 %>  - <%= crole %>
 <% end -%>
+
+lma::elasticsearch::number_of_replicas: <%= @number_of_replicas %>
+lma::elasticsearch::minimum_master_nodes: <%= @minimum_master_nodes %>
+lma::elasticsearch::recover_after_time: <%= @recover_after_time %>
+lma::elasticsearch::recover_after_nodes: <%= @recover_after_nodes %>
 ')
 
 file {$hiera_dir:
