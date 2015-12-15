d4341be90c5b95869d628e77070a6a770f085245
Swann Croiset <scroiset@mirantis.com>
2015-12-15 16:29:59 +0100

    Handle advanced settings for Elasticsearch
    These parameters are configured with default values if not provided:
    * number_of_replicas
    * minimum_master_nodes
    * recover_after_time
    * recover_after_nodes
    
    Change-Id: I3f4c1135c61b209e6e7b6160142c147721306cf9

--- deployment_scripts/puppet/manifests/kibana.pp
+++ deployment_scripts/puppet/manifests/kibana.pp
@@ -14,9 +14,8 @@
 #
 prepare_network_config(hiera('network_scheme', {}))
 $mgmt_address = get_network_role_property('management', 'ipaddr')
-$elasticsearch_kibana = hiera('elasticsearch_kibana')
 
 class { 'lma_logging_analytics::kibana':
-  number_of_replicas => 0 + $elasticsearch_kibana['number_of_replicas'],
+  number_of_replicas => hiera('lma::elasticsearch::number_of_replicas'),
   es_host            => $mgmt_address,
 }
