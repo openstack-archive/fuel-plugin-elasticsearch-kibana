d446acdf4afa77223f406ff6da2fa48dd1d5eb88
Swann Croiset <scroiset@mirantis.com>
2016-01-04 10:06:31 +0100

    Improve cluster deployment
    - Add firewall rules for corosync communication
    - Use dedicated cluster.pp manifest to allow the deployment of coexistant
      clusters for LMA plugins
    - Use primary role property
    
    Implements: elasticsearch-clustering
    
    Change-Id: Ibf4c1c4e62f214725875869621b40a3ef4c20e53

---
+++ deployment_scripts/puppet/manifests/cluster.pp
@@ -0,0 +1,60 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# Compared to the osnailyfacter/modular/cluster/cluster.pp manifest, this task
+# supports the use case where the Pacemaker cluster is made of several
+# unrelated roles.
+prepare_network_config(hiera_hash('network_scheme'))
+
+$corosync_nodes = corosync_nodes(
+    get_nodes_hash_by_roles(
+      hiera_hash('network_metadata'),
+      hiera_array('lma::corosync_roles')
+    ),
+    'mgmt/corosync'
+)
+
+class { 'cluster':
+  internal_address => get_network_role_property('mgmt/corosync', 'ipaddr'),
+  corosync_nodes   => $corosync_nodes,
+}
+
+pcmk_nodes { 'pacemaker' :
+  nodes               => $corosync_nodes,
+  add_pacemaker_nodes => false,
+}
+
+Service <| title == 'corosync' |> {
+  subscribe => File['/etc/corosync/service.d'],
+  require   => File['/etc/corosync/corosync.conf'],
+}
+
+Service['corosync'] -> Pcmk_nodes<||>
+Pcmk_nodes<||> -> Service<| provider == 'pacemaker' |>
+
+# Sometimes during first start pacemaker can not connect to corosync
+# via IPC due to pacemaker and corosync processes are run under different users
+if($::operatingsystem == 'Ubuntu') {
+  $pacemaker_run_uid = 'hacluster'
+  $pacemaker_run_gid = 'haclient'
+
+  file {'/etc/corosync/uidgid.d/pacemaker':
+    content =>"uidgid {
+   uid: ${pacemaker_run_uid}
+   gid: ${pacemaker_run_gid}
+}"
+  }
+
+  File['/etc/corosync/corosync.conf'] -> File['/etc/corosync/uidgid.d/pacemaker'] -> Service <| title == 'corosync' |>
+}
