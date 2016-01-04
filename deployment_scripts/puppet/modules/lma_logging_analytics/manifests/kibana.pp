2c9c0876bd459c3a41aca8d6434b1022c3abd17e
Swann Croiset <scroiset@mirantis.com>
2016-01-04 18:49:06 +0100

    Initialize Elasticsearch data only once
    All Elasticsearch templates and Kibana dashboard imports are performed only
    once from the primary-elasticserach_kibana node.
    
    Implements: blueprint elasticsearch-clustering
    
    Change-Id: Ifa605c9dcdc603080b8adb80dbc71f2796cdf34b

--- deployment_scripts/puppet/modules/lma_logging_analytics/manifests/kibana.pp
+++ deployment_scripts/puppet/modules/lma_logging_analytics/manifests/kibana.pp
@@ -16,11 +16,10 @@
 
 
 class lma_logging_analytics::kibana (
-  $number_of_replicas = $lma_logging_analytics::params::kibana_replicas,
   $es_host = 'localhost',
-) inherits lma_logging_analytics::params {
+) {
 
-  validate_integer($number_of_replicas)
+  include lma_logging_analytics::params
 
   $kibana_dir    = $lma_logging_analytics::params::kibana_dir
   $kibana_conf   = $lma_logging_analytics::params::kibana_config
@@ -45,31 +44,9 @@ class lma_logging_analytics::kibana (
     require => File[$kibana_dir],
   }
 
-  elasticsearch::template { 'kibana':
-    content => "{\"template\":\"kibana-*\", \"settings\": {\"number_of_replicas\":${number_of_replicas}}}",
-    host    => $es_host,
-  }
-
-  # Note that the dashboards are stored in templates/ because it is the only way
-  # for us to read the content of the file. Ideally we would have used the
-  # file() function but until Puppet 3.7 it works only with absolute paths.
-  # See http://www.unixdaemon.net/tools/puppet/puppet-3.7-file-function.html
-  # for details
-  lma_logging_analytics::kibana_dashboard { 'logs':
-    content => template('lma_logging_analytics/kibana_dashboards/logs.json'),
-    host    => $es_host,
-    require => [File["${dashboard_dir}/logs.json"], Elasticsearch::Template['kibana']],
-  }
-
   file { "${dashboard_dir}/notifications.json":
     content => template('lma_logging_analytics/kibana_dashboards/notifications.json'),
     require => File[$kibana_dir],
-  }
-
-  lma_logging_analytics::kibana_dashboard { 'notifications':
-    content => template('lma_logging_analytics/kibana_dashboards/notifications.json'),
-    host    => $es_host,
-    require => [File["${dashboard_dir}/notifications.json"], Elasticsearch::Template['kibana']],
   }
 
   # Install nginx
