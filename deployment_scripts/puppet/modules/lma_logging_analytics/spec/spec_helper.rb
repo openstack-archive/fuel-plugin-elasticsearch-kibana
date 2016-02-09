022673e1ef9ab81383cf7a6c73bfeadfb6902295
Swann Croiset <scroiset@mirantis.com>
2016-02-09 11:21:59 +0100

    Configure noop hiera conf
    This avoids the CI to override out tests
    
    Change-Id: I7cc29065609aee73d2d830690f523f9db63378fe
    (cherry picked from commit b52a8310b695a226b91fd2b69f714d8d4555c5db)

--- deployment_scripts/puppet/modules/lma_logging_analytics/spec/spec_helper.rb
+++ deployment_scripts/puppet/modules/lma_logging_analytics/spec/spec_helper.rb
@@ -19,4 +19,5 @@ RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.environmentpath = File.join(Dir.pwd, 'spec')
+  c.hiera_config = File.join(fixture_path, 'hiera.yaml')
 end
