ba2c4eaf6b8f50e4369f2a7c0266e20f473c9653
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-04-01 15:12:21 +0200

    Fix corosync_nodes usage
    In Fuel 9.0 corosync module interface has been changed. We need to
    adjust our modules accordingly.
    
    Change-Id: If40e96a7807edc13a4d7e8cdfb3a37fa643edf23

--- deployment_scripts/puppet/manifests/enable_quorum.pp
+++ deployment_scripts/puppet/manifests/enable_quorum.pp
@@ -17,6 +17,7 @@ prepare_network_config(hiera('network_scheme', {}))
 $corosync_roles = hiera_array('lma::corosync_roles')
 $network_metadata = hiera('network_metadata')
 $nodes = get_nodes_hash_by_roles($network_metadata, $corosync_roles)
+$fuel_version = 0 + hiera('fuel_version')
 
 if count($nodes) > 2 {
   $policy = 'stop'
@@ -24,10 +25,17 @@ if count($nodes) > 2 {
   $policy = 'ignore'
 }
 
-# The default provider ('pcs') fails to change the property's value so we have
-# to force the use of 'crm'
-cs_property { 'no-quorum-policy':
-  ensure   => present,
-  value    => $policy,
-  provider => 'crm',
+if $fuel_version < 9.0 {
+  # The default provider ('pcs') fails to change the property's value so we have
+  # to force the use of 'crm'
+  cs_property { 'no-quorum-policy':
+    ensure   => present,
+    value    => $policy,
+    provider => 'crm',
+  }
+} else {
+  pcmk_property { 'no-quorum-policy':
+    ensure => present,
+    value  => $policy,
+  }
 }
