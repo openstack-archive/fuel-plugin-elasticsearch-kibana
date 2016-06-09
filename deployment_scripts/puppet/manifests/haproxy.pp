cca7a8098aa7dfe942d14bc028f302055f296b36
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-06-09 18:10:35 +0200

    Enable secure communication over HTTPS
    This patch configures HAProxy to terminate SSL connection when the
    support for secure communications is enabled.
    
    Change-Id: Icdff278875d7daf928fa036ec1e4905205791546
    Implements: blueprint support-secure-communication

--- deployment_scripts/puppet/manifests/haproxy.pp
+++ deployment_scripts/puppet/manifests/haproxy.pp
@@ -22,6 +22,8 @@ $vip = hiera('lma::elasticsearch::vip')
 $nodes_ips = hiera('lma::elasticsearch::nodes')
 $nodes_names = prefix(range(1, size($nodes_ips)), 'server_')
 
+$elasticsearch_kibana = hiera_hash('elasticsearch_kibana')
+
 Openstack::Ha::Haproxy_service {
   server_names        => $nodes_names,
   ipaddresses         => $nodes_ips,
@@ -44,14 +46,55 @@ openstack::ha::haproxy_service { $es_haproxy_service:
   }
 }
 
-openstack::ha::haproxy_service { 'kibana':
-  order                  => '921',
-  listen_port            => $kibana_frontend_port,
-  balancermember_port    => $kibana_backend_port,
-  balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
-  haproxy_config_options => {
-    'option'  => ['httplog', 'http-keep-alive', 'prefer-last-server', 'dontlog-normal'],
-    'balance' => 'roundrobin',
-    'mode'    => 'http',
+if $elasticsearch_kibana['tls_enabled'] {
+  $cert_file = $elasticsearch_kibana['cert_file_path']
+
+  file {$cert_file:
+    ensure  => present,
+    content => $elasticsearch_kibana['kibana_ssl_cert']['content']
+  }
+
+  # We don't use the resource openstack::ha::haproxy_service because we cannot
+  # modify the parameter used to configure SSL in HAProxy and we are not using
+  # the same parameters than the ones used for OpenStack services.
+  include openstack::ha::haproxy_restart
+
+  $bind_address = suffix(any2array($vip), ":${kibana_frontend_port}")
+  $bind = array_to_hash($bind_address, ['ssl', 'crt', $cert_file])
+
+  # Configure HAProxy to listen
+  haproxy::listen { 'kibana':
+    order       => '921',
+    bind        => $bind,
+    options     => {
+      'option'  => ['httplog', 'http-keep-alive', 'prefer-last-server', 'dontlog-normal'],
+      'balance' => 'roundrobin',
+      'mode'    => 'http',
+    },
+    use_include => true,
+    notify      => Exec['haproxy-restart'],
+  }
+
+  haproxy::balancermember { 'kibana':
+    order             => '921',
+    listening_service => $name,
+    server_names      => $nodes_names,
+    ipaddresses       => $nodes_ips,
+    ports             => $kibana_backend_port,
+    options           => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
+    use_include       => true,
+    notify            => Exec['haproxy-restart'],
+  }
+} else {
+  openstack::ha::haproxy_service { 'kibana':
+    order                  => '921',
+    listen_port            => $kibana_frontend_port,
+    balancermember_port    => $kibana_backend_port,
+    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
+    haproxy_config_options => {
+      'option'  => ['httplog', 'http-keep-alive', 'prefer-last-server', 'dontlog-normal'],
+      'balance' => 'roundrobin',
+      'mode'    => 'http',
+    }
   }
 }
