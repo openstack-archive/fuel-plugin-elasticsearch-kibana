b370cbac49860f3c2a8cd909140f305d02ef3bb0
Swann Croiset <scroiset@mirantis.com>
2016-10-11 13:42:03 +0200

    Create ES templates earlier
    The LMA collectors can push logs before the index template creation
    which leads to have the first index with a wrong number_of_replicas and
    wron field indexation settings.
    
    Change-Id: Id166f6e0aea67b60c5c38cc6a4a69ece4928634e

--- deployment_scripts/puppet/manifests/kibana_index_configuration.pp
+++ deployment_scripts/puppet/manifests/kibana_index_configuration.pp
@@ -17,6 +17,7 @@ notice('fuel-plugin-elasticsearch-kibana: kibana_index_configuration.pp')
 $vip = hiera('lma::elasticsearch::vip')
 $es_port = hiera('lma::elasticsearch::rest_port')
 $kibana_index = hiera('lma::elasticsearch::kibana_index')
+$number_of_replicas = hiera('lma::elasticsearch::number_of_replicas')
 
 # Elasticsearch must be reachable through HAproxy before the template creation.
 # This is due the fact that The Elasticsearch Puppet module miserably fails
@@ -39,6 +40,14 @@ lma_logging_analytics::es_template { 'kibana4':
   index_template => $kibana_index,
   require        => Haproxy_backend_status['elasticsearch'],
 }
+
+lma_logging_analytics::es_template { ['log', 'notification']:
+  number_of_replicas => $number_of_replicas,
+  host               => $vip,
+  port               => $es_port,
+  require            => Haproxy_backend_status['elasticsearch'],
+}
+
 
 # Import all Kibana objects in one time by issuing a Bulk request
 class { 'lma_logging_analytics::kibana_dashboards':
